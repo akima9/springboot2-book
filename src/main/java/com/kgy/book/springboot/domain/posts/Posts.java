c63c8e02a0d59236d719cfdb2ecd6de09d4e51a8
akima9 <akima9a@gmail.com>
2020-02-11 14:00:08 +0900

    구글 로그인 연동 시작

--- src/main/java/com/kgy/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/kgy/book/springboot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.kgy.book.springboot.domain.posts;
 
+import com.kgy.book.springboot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,7 +13,7 @@ import javax.persistence.*;
 //@Entity : 테이블과 링크될 클래스임을 나타냄, 클래스의 카멜케이스 이름을 언더스코어 네이밍으로 테이블 이름을 매칭함
 @Entity
 //Posts 클래스는 실제 DB 의 테이블과 매칭될 클래스
-public class Posts {
+public class Posts extends BaseTimeEntity {
     //@Id : 해당 테이블의 PK 필드를 나타냄
     @Id
     //@GeneratedValue : PK 의 생성 규칙을 나타냄, 스프링 부트 2.0 에서는 GenerationType.IDENTITY 옵션을 추가해야만
